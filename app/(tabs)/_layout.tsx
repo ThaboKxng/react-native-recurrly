4b456d804049982cc68ea1faa835ae900f1ecc19
ThaboDube <thaboj082@gmail.com>
2026-05-07 16:35:06 +0200

    Tab Navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import { tabs } from "@/constants/data";
+import { clsx } from "clsx";
+import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
+
+const TabLayout = () => {
+  const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+          <View className="tabs-icon-path" />
+        </View>
+      </View>
+    );
+  };
+  return (
+    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={tab.icon} />
+            ),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+};
+
+export default TabLayout;
